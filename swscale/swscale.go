eaf0f23a9516828aacb813d5f636a6220b2bb6ca
Aaron Marburg <amarburg@apl.washington.edu>
2016-12-23 18:03:45 -0800

    Changd function args for SwsScale...

--- swscale/swscale.go
+++ swscale/swscale.go
@@ -61,13 +61,13 @@ func SwsIssupportedendiannessconversion(p PixelFormat) int {
 }
 
 ////Scale the image slice in srcSlice and put the resulting scaled slice in the image in dst.
-func SwsScale(ctxt *Context, src *uint8, str int, y, h int, d *uint8, ds int) int {
+func SwsScale(ctxt *Context, src [8]*uint8, str [8]int32, y, h int, d [8]*uint8, ds [8]int32) int {
 	cctxt := (*C.struct_SwsContext)(unsafe.Pointer(ctxt))
-	csrc := (*C.uint8_t)(unsafe.Pointer(src))
+	csrc := (**C.uint8_t)(unsafe.Pointer(&src[0]))
 	cstr := (*C.int)(unsafe.Pointer(&str))
-	cd := (*C.uint8_t)(unsafe.Pointer(d))
+	cd := (**C.uint8_t)(unsafe.Pointer(&d[0]))
 	cds := (*C.int)(unsafe.Pointer(&ds))
-	return int(C.sws_scale(cctxt, &csrc, cstr, C.int(y), C.int(h), &cd, cds))
+	return int(C.sws_scale(cctxt, csrc, cstr, C.int(y), C.int(h), cd, cds))
 }
 
 func SwsSetcolorspacedetails(ctxt *Context, it *int, sr int, t *int, dr, b, c, s int) int {
