b11dab2bd08557c1d0dc3ce5c4ba3fc036b3b0e9
Aaron Marburg <amarburg@apl.washington.edu>
2016-12-23 18:01:14 -0800

    Added RGB and RGBA pixel formats (it's a start)

--- avcodec/pixelformat.go
+++ avcodec/pixelformat.go
@@ -10,6 +10,11 @@ import (
 	"unsafe"
 )
 
+const (
+	AV_PIX_FMT_RGB24 = int(C.AV_PIX_FMT_RGB24)
+	AV_PIX_FMT_RGBA  = int(C.AV_PIX_FMT_RGBA)
+)
+
 //Utility function to access log2_chroma_w log2_chroma_h from the pixel format AvPixFmtDescriptor.
 func (p PixelFormat) AvcodecGetChromaSubSample(h, v *int) {
 	C.avcodec_get_chroma_sub_sample((C.enum_AVPixelFormat)(p), (*C.int)(unsafe.Pointer(h)), (*C.int)(unsafe.Pointer(v)))
