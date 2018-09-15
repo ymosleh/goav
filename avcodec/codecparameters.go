88bf86c8ac295297f19a70b4ce6b669a8db5d14f
Quentin Renard <contact@asticode.com>
2018-09-15 18:22:05 +0200

    Added AvcodecParametersCopy

--- avcodec/codecparameters.go
+++ avcodec/codecparameters.go
@@ -10,3 +10,7 @@ func AvcodecParametersToContext(codecContext *Context, codecParameters *CodecPar
 func AvcodecParametersFromContext(codecParameters *CodecParameters, codecContext *Context) int {
 	return int(C.avcodec_parameters_from_context((*C.struct_AVCodecParameters)(codecParameters), (*C.struct_AVCodecContext)(codecContext)))
 }
+
+func AvcodecParametersCopy(out, in *CodecParameters) int {
+	return int(C.avcodec_parameters_copy((*C.struct_AVCodecParameters)(out), (*C.struct_AVCodecParameters)(in)))
+}
