ae29cc50dd929e085e0524f0970c3fd2167c647e
Quentin Renard <contact@asticode.com>
2018-09-22 13:48:01 +0200

    Added AvDictParseString

--- avutil/dictionary.go
+++ avutil/dictionary.go
@@ -16,6 +16,10 @@ func AvDictSet(d **Dictionary, key, value string, flags int) int {
 	return int(C.av_dict_set((**C.struct_AVDictionary)(unsafe.Pointer(d)), C.CString(key), C.CString(value), C.int(flags)))
 }
 
+func AvDictParseString(d **Dictionary, i, keyValSep, pairsSep string, flags int) int {
+	return int(C.av_dict_parse_string((**C.struct_AVDictionary)(unsafe.Pointer(d)), C.CString(i), C.CString(keyValSep), C.CString(pairsSep), C.int(flags)))
+}
+
 func AvDictGet(d *Dictionary, key string, prev *DictionaryEntry, flags int) *DictionaryEntry {
 	return (*DictionaryEntry)(C.av_dict_get((*C.struct_AVDictionary)(d), C.CString(key), (*C.struct_AVDictionaryEntry)(prev), C.int(flags)))
 }
