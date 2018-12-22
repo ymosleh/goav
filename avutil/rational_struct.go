990eb7211e77a93c7581c02552d8dc02c0804e13
Quentin Renard <contact@asticode.com>
2018-12-22 09:21:06 +0100

    Updated rational non pointer functions

--- avutil/rational_struct.go
+++ avutil/rational_struct.go
@@ -3,7 +3,7 @@ package avutil
 //#include <libavutil/rational.h>
 import "C"
 
-func (r *Rational) Num() int {
+func (r Rational) Num() int {
 	return int(r.num)
 }
 
@@ -11,7 +11,7 @@ func (r *Rational) SetNum(num int) {
 	r.num = C.int(num)
 }
 
-func (r *Rational) Den() int {
+func (r Rational) Den() int {
 	return int(r.den)
 }
 
@@ -19,7 +19,7 @@ func (r *Rational) SetDen(den int) {
 	r.den = C.int(den)
 }
 
-func (r *Rational) ToDouble() float64 {
+func (r Rational) ToDouble() float64 {
 	return float64(r.Num()) / float64(r.Den())
 }
 
