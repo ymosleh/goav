7671faa1f2042b5891117ebf5cb240f73e09c09b
Quentin Renard <contact@asticode.com>
2020-10-27 17:08:31 +0100

    Added avlog set callback

--- avutil/log.go
+++ avutil/log.go
@@ -2,6 +2,25 @@ package avutil
 
 //#cgo pkg-config: libavutil
 //#include <libavutil/log.h>
+/*
+extern void goAvLogCustomCallback(int level, char* msg);
+
+static inline void avLogCustomCallback(void *avcl, int level, const char *fmt, va_list vl)
+{
+	if (level > av_log_get_level()) return;
+	char msg[1024];
+	vsprintf(msg, fmt, vl);
+	goAvLogCustomCallback(level, msg);
+}
+static inline void setAvLogCustomCallback()
+{
+	av_log_set_callback(avLogCustomCallback);
+}
+static inline void resetAvLogCallback()
+{
+	av_log_set_callback(av_log_default_callback);
+}
+*/
 import "C"
 
 // Logging constants
@@ -24,4 +43,28 @@ func AvLogGetLevel() int {
 // AvLogSetLevel sets the log level.
 func AvLogSetLevel(level int) {
 	C.av_log_set_level(C.int(level))
+}
+
+// AvLogCallback represents a log callback
+type AvLogCallback func(level int, msg string)
+
+var avLogCallback AvLogCallback
+
+// AvLogSetCallback sets the log callback to a custom callback
+func AvLogSetCallback(c AvLogCallback) {
+	avLogCallback = c
+	C.setAvLogCustomCallback()
+}
+
+//export goAvLogCustomCallback
+func goAvLogCustomCallback(level C.int, msg *C.char) {
+	if avLogCallback == nil {
+		return
+	}
+	avLogCallback(int(level), C.GoString(msg))
+}
+
+// AvLogResetCallback resets the log callback to the default callback
+func AvLogResetCallback() {
+	C.resetAvLogCallback()
 }
